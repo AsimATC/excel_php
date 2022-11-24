bfc9a74f4688b21142eb489cdabb69bf36b13a09
AsimAtıcı <asimatici51@gmail.com>
2022-11-24 16:55:40 +0300

    excel upload finigs

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<?php include "db.php"; ?>
+
 <!doctype html>
-<html lang="en">
+<html lang="tr">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Data Table</title>
+    <title>Excel Upload</title>
 
     <!-- Table Data -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
@@ -20,17 +22,64 @@
 
     <div class="container py-5">
 
+        <!-- Upload Alert Row -->
+        <div class="row">
+            <div class="col-md-12">
+
+                <?php
+
+                use Shuchkin\SimpleXLSX;
+
+                if (isset($_POST['yukle'])) {
+
+                    // File Upload
+                    if ($_FILES['excel_file']['error'] == 0) {
+                        $gecici_isim  = $_FILES['excel_file']['tmp_name'];
+                        $dosya_ismi = $_FILES['excel_file']['name'];
+                        $sayi = rand(1000, 9999);
+                        $isim = $sayi . $dosya_ismi;
+                        move_uploaded_file($gecici_isim, "gecici/isim");
+
+                        include_once "excel.php";
+
+                        // File Upload Control
+                        if ($xlsx = SimpleXLSX::parse("gecici/isim")) {
+
+                            // Excel Convert to VT
+                            foreach ($xlsx->rows() as $key => $satir) {
+                                $sorgu = $db->prepare(" INSERT INTO ogrenciler SET
+                                isim = :isim,
+                                mail = :mail,
+                                Telefon = :Telefon
+                                 ");
+                                $sorgu->execute([
+                                    'isim' => $satir[0],
+                                    'mail' => $satir[1],
+                                    'Telefon' => $satir[2]
+                                ]);
+                            }
+
+                        } else {
+                            echo SimpleXLSX::parseError(); // başarısız
+                        }
+                    }
+                }
+
+                ?>
+
+            </div>
+        </div>
+
+        <!-- Excel button -->
         <div class="row mb-3">
             <div class="col-md-12 text-end">
-                <!-- Button trigger modal -->
                 <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     EXCEL UPLOAD
                 </button>
             </div>
-
-
         </div>
 
+        <!-- List Table -->
         <div class="row">
             <table class="table" id="myTable">
                 <thead class="bg-dark text-white">
@@ -42,24 +91,28 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Asım</td>
-                        <td>Atıcı</td>
-                        <td>Asim@tonyukukajans.com</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Mehmet</td>
-                        <td>Yoldakalan</td>
-                        <td>Mehmet@tonyukukajans.com</td>
-                    </tr>
+                    <?php
+
+                    $ogrenciler = $db->prepare("SELECT * FROM  ogrenciler ");
+                    $ogrenciler->execute(array());
+
+                    while ($ogrenciyaz = $ogrenciler->fetch(PDO::FETCH_ASSOC)) {
+
+                    ?>
+                        <tr>
+                            <th scope="row"><?php echo $ogrenciyaz['id'] ?></th>
+                            <td><?php echo $ogrenciyaz['isim'] ?></td>
+                            <td><?php echo $ogrenciyaz['mail'] ?></td>
+                            <td><?php echo $ogrenciyaz['Telefon'] ?></td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
+
     </div>
 
-    <!-- Modal -->
+    <!-- Upload Excel -->
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
@@ -69,11 +122,12 @@
                 </div>
                 <div class="modal-body">
                     <form action="" method="POST" enctype="multipart/form-data">
-                        <input type="file" class="form-control">
+                        <input type="file" name="excel_file" class="form-control">
+                        <button class="btn btn-primary mt-3 w-50 mx-auto d-block" name="yukle">YÜKLE</button>
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" name="yukle" class="btn btn-warning" data-bs-dismiss="modal">Çıkış</button>
+                    <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Çıkış</button>
                 </div>
             </div>
         </div>
